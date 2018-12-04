67b23fd7c210981ab1d34948e077221da3f09d2d
ALEXANDRE MARINHO DE OLIVEIRA <alexmdo@gmail.com>
2018-12-04 09:03:38 -0300

    Adicionando o menu na App Bar

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.java
@@ -1,9 +1,14 @@
 package br.com.alura.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -21,6 +26,17 @@ public class ListaNotasActivity extends AppCompatActivity {
 
         List<Nota> listaNotas = mockarNotas();
         configurarRecyclerView(listaNotas);
+
+        TextView insereNotas = findViewById(R.id.lista_notas_insere_nota);
+        insereNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irParaFormularioNotaActivity = new Intent(this, FormularioNotaActivity.class);
+                startActivity(irParaFormularioNotaActivity);
+            }
+        });
+
+
     }
 
     private void configurarRecyclerView(List<Nota> listaNotas) {
