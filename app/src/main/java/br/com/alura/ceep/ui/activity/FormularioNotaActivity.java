20cbafa661525e6bf1274ae5ad377d99c9ba9665
ALEXANDRE MARINHO DE OLIVEIRA <alexmdo@gmail.com>
2018-12-04 09:08:41 -0300

    Adicionando o menu na App Bar

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import br.com.alura.ceep.R;
 
@@ -11,5 +13,13 @@ public class FormularioNotaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_formulario_nota_salva, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
